ae380f41bd610d4e8d1076101662752cdce4d49a
Fernando Cotrena <fernando.cotrena@xcaleconsulting.com>
2022-06-09 09:25:27 -0300

    nuevo test para implementar

---
+++ src/test/java/IFeeLuckyTest.java
@@ -0,0 +1,10 @@
+public class IFeeLuckyTest {
+
+    /*Ingresar a google.com.ar
+    Ingresar “doodles”  en el buscador y continuar con la opción “Me siento con suerte”
+    Ingresar a “Acerca de”
+    Validar que se muestre el año 1998 en la fecha de creación de doodles
+*/
+
+
+}
