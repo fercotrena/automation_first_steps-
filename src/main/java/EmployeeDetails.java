26a0db1dc5a4841515214006521a9666c8c67bc9
Fernando Cotrena <fernando.cotrena@xcaleconsulting.com>
2022-06-09 15:22:12 -0300

    ejercicio de java

---
+++ src/main/java/EmployeeDetails.java
@@ -0,0 +1,22 @@
+public class EmployeeDetails {
+
+  /*
+    1)
+    Agregar a la clave los atributos que nos permitan guardar los datos de:
+    Nombre,piso,comision,piso,oficina,legajo, fecha de nacimiento
+
+    EJ:
+    Nombre: Pedro
+    Comision: 3.5
+    Piso: 3
+    Oficina: A
+    Legajo: XC002
+    Fecha de nacimiento: 10-01-1985
+
+    2) agregar un metodo que permita conocer la edad del employed.
+
+   */
+
+
+
+}
---
+++ src/main/java/Run1.java
@@ -0,0 +1,17 @@
+public class Run1 {
+    public static void main(String[] args) {
+
+     /*
+     * 1. completar los puntos de la clase EmployeeDatails
+     * 2. Crear dos objectos de la clase.
+     * 3. Imprimir la edad del primero
+     * 4. Compara los dos objetos creados y determinar si son iguales. Se consideran que iguales
+     * si comparten el mismo piso, departamento y comision.
+     * 5. Imprimir todos los datos del primer objeto.
+     *      *
+     */
+
+    }
+
+
+}
